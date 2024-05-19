82bd48c9464b34136ea93c1c1ba923cad1136f5a
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2024-05-19 21:44:23 +0900

    Add files via upload

--- API-Nasa/consulting_urls.py
+++ API-Nasa/consulting_urls.py
@@ -2,12 +2,13 @@ from tkinter import *
 
 import webview
 import webbrowser
+import urllib.request
 
 
 #  ----------------------------------------------------------------------------
 
 
-# Using webview
+#  --   [[[ Using webview ]]]
 
 
 def page_url_in_tkinter(url_test):
@@ -18,14 +19,17 @@ def page_url_in_tkinter(url_test):
     webview.start()
 
 
+url_image = "https://apod.nasa.gov/apod/image/2405/AuroraStartrails_chiragupreti1024.jpg"
+#  page_url_in_tkinter(url_image)
+
+
 #  ----------------------------------------------------------------------------
 
 
-# Using web browser(sample)
+#  --   [[[ Using web browser(sample) ]]]
 
 
 def url_open(url_test):
-
     root = Tk()
     root.title("URL open on browser (WebBrowsers)")
     root.geometry("400x50")
@@ -37,13 +41,29 @@ def url_open(url_test):
     root.mainloop()
 
 
+page_test = "https://api.nasa.gov/"
+#  url_open(page_test)
+
+
 #  ----------------------------------------------------------------------------
 
 
-page_test = "https://api.nasa.gov/"
-url_open(page_test)
+#  --   [[[ Using urllib ]]]
 
 
-url_image = "https://apod.nasa.gov/apod/image/2405/AuroraStartrails_chiragupreti1024.jpg"
-page_url_in_tkinter(url_image)
+def save_image_from_url(url_test):
+
+    response = urllib.request.urlopen(url_test)
+
+    with open("AuroraStartrails_chiragupreti1024.jpg", "wb") as file:
+        chunk_size = 4096
+        while True:
+            chunk = response.read(chunk_size)
+            if not chunk:
+                break
+            file.write(chunk)
+
+
+url_to_save = "https://apod.nasa.gov/apod/image/2405/AuroraStartrails_chiragupreti1024.jpg"
+#  save_image_from_url(url_to_save)
 
--- API-Nasa/main.py
+++ API-Nasa/main.py
@@ -1,30 +1,40 @@
 import json
 import requests
 
-
 api_key = "NgtvBLp13upL4FK2z6qyWSRJNcJ3U1w4fJcokZmi"
 demo_key = "DEMO_KEY"
 
+address = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08"
+earth_ad = "https://api.nasa.gov/planetary/earth/assets?lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10"
 
-address = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key="
+neo_browse= "https://api.nasa.gov/neo/rest/v1/neo/browse"
 
 
 def testing_request(address_captured, key):
-    request = requests.get(f"{address_captured}{key}")
+    key_used = '?api_key={}'.format(key)
+
+    request = requests.get(f"{address_captured}{key_used}", )
     required = json.loads(request.text)
 
     return required
 
 
-requided = testing_request(address, demo_key)
-
+requided = testing_request(neo_browse, demo_key)
 
 for i in requided:
     print(i)
     print(type(requided[i]))
     print(requided[i])
+    print(len(requided[i]))
     print('\n')
 
+    if i == 'near_earth_objects':
+        dict_keys = set()
+        print('\n\n\n')
+        for j in requided[i]:
+            for z in j:
+                dict_keys.add(z)
 
-print('\n\n\n**', type(requided), len(requided))
+for i in dict_keys:
+    print(i)
 
