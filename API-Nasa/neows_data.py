5af24c3f438e429e0327bf5ff013341857ba30dc
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2024-08-09 20:45:01 +0900

    Add files via upload

---
+++ API-Nasa/neows_data.py
@@ -0,0 +1,46 @@
+from api_data.all_neows.Tezcatlipoca import tezcatlipoca_all_data
+from api_data.all_neows.Anteros import anteros_all_data
+from api_data.all_neows.Cuyo import cuyo_all_data
+from api_data.all_neows.Boreas import boreas_all_data
+from api_data.all_neows.Quetzalcoatl import quetzalcoatl_all_data
+from api_data.all_neows.Sisyphus import sisyphus_all_data
+from api_data.all_neows.Cerberus import cerberus_all_data
+from api_data.all_neows.Daedalus import daedalus_all_data
+from api_data.all_neows.Antinous import antinous_all_data
+from api_data.all_neows.Apollo import apollo_all_data
+from api_data.all_neows.Toro import toro_all_data
+from api_data.all_neows.Ivar import ivar_all_data
+from api_data.all_neows.Geographos import geographos_all_data
+from api_data.all_neows.Betulia import betulia_all_data
+from api_data.all_neows.Icarus import icarus_all_data
+from api_data.all_neows.Amor import amor_all_data
+from api_data.all_neows.Ganymed import ganymed_all_data
+from api_data.all_neows.Alinda import alinda_all_data
+from api_data.all_neows.Albert import albert_all_data
+from api_data.all_neows.Eros import eros_all_data
+from objects.NeoWs import NeoWs
+
+
+neows_names = dict()
+
+
+neows_names['433 Eros (A898 PA)'] = NeoWs(eros_all_data)
+neows_names['719 Albert (A911 TB)'] = NeoWs(albert_all_data)
+neows_names['887 Alinda (A918 AA)'] = NeoWs(alinda_all_data)
+neows_names['1036 Ganymed (A924 UB)'] = NeoWs(ganymed_all_data)
+neows_names['1221 Amor (1932 EA1)'] = NeoWs(amor_all_data)
+neows_names['1566 Icarus (1949 MA)'] = NeoWs(icarus_all_data)
+neows_names['1580 Betulia (1950 KA)'] = NeoWs(betulia_all_data)
+neows_names['1620 Geographos (1951 RA)'] = NeoWs(geographos_all_data)
+neows_names['1627 Ivar (1929 SH)'] = NeoWs(ivar_all_data)
+neows_names['1685 Toro (1948 OA)'] = NeoWs(toro_all_data)
+neows_names['1862 Apollo (1932 HA)'] = NeoWs(apollo_all_data)
+neows_names['1863 Antinous (1948 EA)'] = NeoWs(antinous_all_data)
+neows_names['1864 Daedalus (1971 FA)'] = NeoWs(daedalus_all_data)
+neows_names['1865 Cerberus (1971 UA)'] = NeoWs(cerberus_all_data)
+neows_names['1866 Sisyphus (1972 XA)'] = NeoWs(sisyphus_all_data)
+neows_names['1915 Quetzalcoatl (1953 EA)'] = NeoWs(quetzalcoatl_all_data)
+neows_names['1916 Boreas (1953 RA)'] = NeoWs(boreas_all_data)
+neows_names['1917 Cuyo (1968 AA)'] = NeoWs(cuyo_all_data)
+neows_names['1943 Anteros (1973 EC)'] = NeoWs(anteros_all_data)
+neows_names['1980 Tezcatlipoca (1950 LA)'] = NeoWs(tezcatlipoca_all_data)
